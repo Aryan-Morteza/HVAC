67f26787ad68c6d2c570a724f58eef2f201e2050
Aryan <Aryanbeta2003@gmail.com>
2019-05-04 20:24:39 +0430

    2nd commit

--- dynamic-learning.py
+++ dynamic-learning.py
@@ -9,29 +9,27 @@ import time
 import sys
 import datetime
 
-
+# Loading Plant and drop unuseful row (with zero value or nan)	
 df = pd.DataFrame()
 dataset = pd.DataFrame()
-
+data = pd.read_pickle('System_Plant3')
+data = data.dropna()
+cnt = 0 
+dataset_size = 0 														#size of dataset
+start_day_skipper = 0 													#indicate the days we need to skip
+dataset_size_limit = 500 												#indicate limitation size of dataset for learning
+								
+# Learning threat which is learning the Dataset we created before 
 def learning(dataset,date_full):
-	#for row in dataset.iterrows():
-		#if row[1][0] < 0:dataset = dataset.drop([row[0]])
-	mean = 0
+	# Preprocessing for time show in plot
 	date_full = date_full - datetime.timedelta(hours = (date_full.hour - 6))
 	if date_full.minute > 30:
 		date_full = date_full - datetime.timedelta(minutes = 30)
-	#print dataset
-	#print str(datetime.timedelta(minutes=30))
-	#print (str(date_full.hour)+str(date_full.minute))
-	#print dataset.mean()[0]
-	#for row in dataset.iterrows():
-		#mean += row[1][0]
-	#mean = float(mean)/(dataset.size/4)
-	#print mean
 	S = []
 	R = []
 	E = []
 	P = []
+	
 	# Sorting dataframe(dataset) in order to our usage(2 first column for input, 2 last column for output)
 	dataset = dataset[['T0','Delta_t','T1','Delta_T']]
 	X_size = 2
@@ -65,14 +63,12 @@ def learning(dataset,date_full):
 	# Initialize placeholders size of hidden layers
 	X_data = tf.placeholder(shape=[None, X_size], dtype=tf.float32)
 	Y_target = tf.placeholder(shape=[None, Y_size], dtype=tf.float32)
-	#print ((dataset.size/4)/300)+1
 	hidden_layer_nodes = ((dataset.size/4)/400)+1
 	hidden_layer_nodes1 = ((dataset.size/4)/400)+1
 	hidden_layer_nodes2 = ((dataset.size/4)/400)+1
 	hidden_layer_nodes3 = ((dataset.size/4)/400)+1
 	hidden_layer_nodes4 = ((dataset.size/4)/400)+1
-	
-	
+		
 	# Create variables for Neural Network layers(we using 4 hidden layer as we tested)
 	w1 = tf.Variable(tf.random_normal(shape=[X_size,hidden_layer_nodes])) 					# Inputs -> Hidden Layer
 	b1 = tf.Variable(tf.zeros(shape=[hidden_layer_nodes]))   								# First Bias
@@ -86,7 +82,7 @@ def learning(dataset,date_full):
 	b5 = tf.Variable(tf.zeros(shape=[hidden_layer_nodes4]))   								# Fourth Bias
 	w6 = tf.Variable(tf.random_normal(shape=[hidden_layer_nodes4,Y_size])) 					# Hidden layer4 -> Outputs
 	b6 = tf.Variable(tf.zeros(shape=[Y_size]))   											# Last Bias
-	#print sess.run(w1)
+
 	# Operations of hidden layer(all activation funstions are Sigmoid except last one which is linear)
 	hidden_output = tf.sigmoid(tf.add(tf.matmul(X_data, w1), b1))
 	hidden_output1 = tf.sigmoid(tf.add(tf.matmul(hidden_output, w2), b2))
@@ -95,77 +91,59 @@ def learning(dataset,date_full):
 	hidden_output4 = tf.sigmoid(tf.add(tf.matmul(hidden_output3, w5), b5))
 	final_output = tf.add(tf.matmul(hidden_output4, w6),b6)
 	
-	# Cost function is MSE, Optimizer is Adam optimizer as we tested is better thanf Gradiant descent
+	# Cost function is MSE, Optimizer is Adam optimizer as we tested is better than Gradiant descent
 	global_step = tf.Variable(0, trainable=False)
 	learning_rate = tf.train.exponential_decay(learning_rate, global_step,100, 0.96, staircase=True)
 	loss = tf.reduce_mean(tf.square(final_output - Y_target))
 	optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss,global_step)
 	
-	
-	# # Initialize variables(first we try loading weight from Checkpoint)
-	# try:
-		# saver = tf.train.Saver()
-		# saver.restore(sess, "/home/wcl-lab/model1.ckpt")
-		# print("Model restored.")
-	# except:
+	# Initializing the Tensor variable
 	init = tf.global_variables_initializer()
 	sess.run(init)
-	print "Dataset size =", dataset.size/4		
+	print "Dataset size =", dataset.size/4	
+		
 	# Training the model as we setted up
 	print 'Training the model...'
 	i = 0
-	l = 0
-	cnt = 0
-	
+	lrn_cnt = 0
+	lmt_cycle_learning = 3 												#indicate the number of cycle we force dataset to learn
 	while 1:
-		e = 0
-		#optimizer = tf.train.AdamOptimizer(learning_rate).minimize(loss)
 		sess.run(optimizer, feed_dict={X_data: X_train, Y_target: Y_train})
-		# for l in range(len(X_test)):
-			# #print('Actual:', Y_test[i], 'Predicted:', sess.run(final_output, feed_dict={X_data: [X_test[i]]}))
-			# e += abs(Y_test[l]-sess.run(final_output, feed_dict={X_data: [X_test[l]]}))
 		if i % interval == 0:
 			x = sess.run(loss, feed_dict={X_data: X_train, Y_target: Y_train})
-			print "Epoch =",i," | Loss =",x," | Accuracy =",float(e)/len(X_test),"\r",
+			print "Epoch =",i," | Loss =",x,"\r",
 			E.append(float(x))
 			P.append(float(i))
-		# if x < 0.5:
-			# print 'Epoch', i, '|', 'Loss:', x
-			# break 
 			y = x
 		if i == epoch :	
-			print "Epoch =",i," | Loss =",x," | Accuracy =",float(e)/len(X_test)
+			print "Epoch =",i," | Loss =",x
 			if x > 1:
 				learning_rate = 0.1
 				init = tf.global_variables_initializer()
 				sess.run(init)
 				i = 0
-			
+				lrn_cnt += 1
+				if lrn_cnt > lmt_cycle_learning:
+					print "Dataset is not proper for learning!"
+					break
 			else: break	
 		if str(x) == "nan":
 			print('Epoch', i, '|', 'Loss:', x)	
 			print dataset
 			init = tf.global_variables_initializer()
 			sess.run(init)
-			#break
-		#print learning_rate
-		#learning_rate = float(learning_rate)/epoch
 		i += 1	
+	# Plotting error 	
 	plt.plot(P,E)
 	plt.xlabel('Epoch')
 	plt.ylabel('Error')
 	plt.show(block=False)
 	plt.pause(5)
 	plt.close()	
-		# Prediction
-		# for i in range(len(X_test)):
-			# #print('Actual:', Y_test[i], 'Predicted:', sess.run(final_output, feed_dict={X_data: [X_test[i]]}))
-			# print "Error = ", abs(Y_test[i] - sess.run(final_output, feed_dict={X_data: [X_test[i]]}))
-	# saver = tf.train.Saver()
-	# save_path = saver.save(sess, "/home/wcl-lab/model1.ckpt")
-	# print("Model saved in path: %s" % save_path)	
+
+	# Prediction future
 	X_test[0][0] = dataset.iloc[0][0]
-	t = 15
+	t = 15 																#time step horizion
 	X_test[0][1] = t
 	cnt = 0
 	i = 0
@@ -175,55 +153,36 @@ def learning(dataset,date_full):
 	R.append(str(date_full.strftime("%H:%M")))
 	while i != 24:
 		last_pred = sess.run(final_output, feed_dict={X_data: [X_test[0]]})
-		#print last_pred,t,i
 		S.append(float(last_pred + dataset.iloc[0][0]))
 		R.append(str((date_full+datetime.timedelta(minutes=t)).strftime("%H:%M")))
-		pred = 0
-		if i > 1:
-			if last_pred - pred > 0 and last_pred - pred > 0.2:
-				cnt += 1
-				#print last_pred,pred					
-				#if cnt > 5 andlast_pred < A:
-				# #if cnt > 60:
-					# break
-				A = last_pred
-		pred = last_pred	
-
 		t = t + 15
 		X_test[0][0] = dataset.iloc[0][0]
+		#X_test[0][0] = last_pred
 		X_test[0][1] = t 
 		i += 1	
 	print "Date =", str(date_full).split(" ")[0]
-	#print dataset.iloc[0][0],A + dataset.iloc[0][0] ,"Kp =",A,"Tau =",(cnt+1)*15*60,"Seconds",float((cnt+1)*15)/60,"Hours"
 	plt.plot(R,S)
 	plt.xlabel('Time (Minute)')
 	plt.ylabel('Temperature (Celsius)')
 	plt.xticks(rotation=90)
 	plt.show(block=False)
-	#plt.pause(5)
-	#plt.savefig("/home/wcl-lab/Desktop/plots1/"+str(date_full).split(" ")[0]+".pdf", bbox_inches='tight')
-	plt.savefig("/home/aryan/Desktop/plots-cooling/"+str(date_full).split(" ")[0]+".png", bbox_inches='tight')
+	plt.savefig("/home/wcl-lab/Desktop/plots-cooling/"+str(date_full).split(" ")[0]+".pdf", bbox_inches='tight')
+	plt.savefig("/home/wcl-lab/Desktop/plots-cooling/"+str(date_full).split(" ")[0]+".png", bbox_inches='tight')
 	plt.close()
 	tf.reset_default_graph()
 	print "Graph saved."
 	print "[Done!]","\n"
-data = pd.read_pickle('System_Plant3')
-data = data.dropna()
-#print data
-cnt = 0 
-dataset_size = 0 #size of dataset
-start_day_skipper = 0
+	
+# Make Dataset from dataframe for learning the dynamic
 for row in data.iterrows():
-	if row[1][0] == 1:
+	if row[1][0] == 1: 													
 		date_full = row[1][1]
-		#print str(date_full)[8:10]
-		#if int(str(date_full)[8:10]) > 12:
-		if dataset_size > 500:
-			if cnt > start_day_skipper:
+		if dataset_size > dataset_size_limit:
+			if cnt >= start_day_skipper:
+				#print dataset
 				learning(dataset,date_full)
-				dataset.drop(dataset.index, inplace=True)
-				df.drop(df.index, inplace=True)
-			#break
+			dataset.drop(dataset.index, inplace=True)
+			df.drop(df.index, inplace=True)
 			cnt += 1
 			print cnt
 		else:
@@ -233,7 +192,7 @@ for row in data.iterrows():
 	if row[1][1] != 0 and row[1][0] != 1:
 		df = df.append(pd.DataFrame({'Delta_t':[row[1][0]],'T0':[row[1][1]],'T1':[row[1][2]]}), ignore_index=True)	
 	else:
-		if cnt > start_day_skipper:
+		if cnt >= start_day_skipper:
 			for i in xrange(df.size/3):
 				j = i
 				k = 0
@@ -241,9 +200,10 @@ for row in data.iterrows():
 					dataset = dataset.append(pd.DataFrame({'Delta_t':[(k+1)*5],'T0':[df.iloc[i][1]],'T1':[df.iloc[j][2]],'Delta_T':[df.iloc[j][2]-df.iloc[i][1]]}), ignore_index=True)	
 					k = k+1
 					j = j+1	
-					#print dataset
 			dataset_size = dataset.size/4		
-		else: dataset_size = 501
+		else: 
+			dataset_size = dataset_size_limit + 1
+			pass
 
 
 
